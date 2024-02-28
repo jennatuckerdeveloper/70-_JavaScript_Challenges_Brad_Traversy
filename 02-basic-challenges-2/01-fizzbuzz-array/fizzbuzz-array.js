0497cdd8702d6bb573e3fc7ac53ff7a5e7f71f92
Jenna Tucker <jennatuckerdeveloper@gmail.com>
2024-02-27 18:29:35 -0800

    challenge 1

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,17 @@
-function fizzBuzzArray() {}
+// You have to leverage the spread operator to map this.  I did not remember that!
+const rangeArr = (num) => [...new Array(num)].map((_, i) => i + 1)
 
-module.exports = fizzBuzzArray;
+const isDivisibleBy = (div, num) => !(num % div)
+
+const makeFizzBuzzString = (isDivisibleBy3, isDivisibleBy5) =>
+  `${isDivisibleBy3 ? 'Fizz' : ''}` + `${isDivisibleBy5 ? 'Buzz' : ''}`
+
+const fizzBuzzArray = (count) =>
+	rangeArr(count).map((num) => {
+		const isDivisibleBy3 = isDivisibleBy(3, num)
+		const isDivisibleBy5 = isDivisibleBy(5, num)
+		const fizzBuzzStr = makeFizzBuzzString(isDivisibleBy3, isDivisibleBy5)
+		return !!fizzBuzzStr ? fizzBuzzStr : num
+	})
+
+module.exports = fizzBuzzArray
