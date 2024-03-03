edabf7db989b6ce56b5d7ccb33e4818636ddd9b5
Jenna Tucker <jennatuckerdeveloper@gmail.com>
2024-03-02 22:54:59 -0800

    section2/challenge7

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,16 @@
-function findFirstNonRepeatingCharacter() {}
+const findFirstNonRepeatingCharacter = (str) => {
+	const chars = str.split('')
+	// To prevent running the duplicated chars multiple times.
+	const uniqueChars = Array.from(new Set(str.split('')))
+	let foundNonRepeating = null
+	// Using a for loop instead of forEach to break when found.
+	for (let char of uniqueChars) {
+		if (chars.filter((e) => e === char).length === 1) {
+			foundNonRepeating = char
+			break
+		}
+	}
+	return foundNonRepeating
+}
 
-module.exports = findFirstNonRepeatingCharacter;
+module.exports = findFirstNonRepeatingCharacter
