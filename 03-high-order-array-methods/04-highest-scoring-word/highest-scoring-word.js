ea1782953cdd7881f4051368602ce00572acd098
Jenna Tucker <jennatuckerdeveloper@gmail.com>
2024-03-08 01:01:32 -0800

    section3/challenge04

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,20 @@
-function highestScoringWord() {}
+const highestScoringWord = (wordStr) => {
+	const words = wordStr.split(' ')
+	const scores = []
+	for (let i in words) {
+		scores[i] = words[i]
+			.split('')
+			.reduce((score, letter) => score + letter.charCodeAt(0) - 96, 0)
+	}
+	let highest = 0
+	let highestIndex = 0
+	for (let i in scores) {
+		if (scores[i] > highest) {
+			highest = scores[i]
+			highestIndex = Number(i)
+		}
+	}
+	return words[highestIndex]
+}
 
-module.exports = highestScoringWord;
+module.exports = highestScoringWord
