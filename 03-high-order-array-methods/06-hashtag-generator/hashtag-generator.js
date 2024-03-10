692b1497dda6f0ca47d706f840b69724cebd0e3d
Jenna Tucker <jennatuckerdeveloper@gmail.com>
2024-03-09 22:43:10 -0800

    section3/solution6

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,15 @@
-function generateHashtag() {}
+const capitalizeFirst = (str) => str.slice(0, 1).toUpperCase() + str.slice(1)
 
-module.exports = generateHashtag;
+const generateHashtag = (str) => {
+	if (!str) return false
+	if (str.length > 140) return false
+	return (
+		'#' +
+		str
+			.split(' ')
+			.map((word) => capitalizeFirst(word))
+			.join('')
+	)
+}
+
+module.exports = generateHashtag
