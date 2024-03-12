71db6c39fd3a3327d1f32da8033bfdd8f0b670c5
Jenna Tucker <jennatuckerdeveloper@gmail.com>
2024-03-11 22:30:18 -0700

    section3/challenge8

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,24 @@
-function analyzeCarMileage() {}
+const analyzeCarMileage = (cars) => {
+	const carData = cars.reduce(
+		(carData, car) => {
+			if (carData.highestMileageCar.mileage < car.mileage)
+				carData.highestMileageCar = car
+			if (carData.lowestMileageCar.mileage > car.mileage)
+				carData.lowestMileageCar = car
+			carData.totalMileage = carData.totalMileage + car.mileage
+			return carData
+		},
+		{
+			highestMileageCar: cars[0],
+			lowestMileageCar: cars[0],
+			totalMileage: 0
+		},
+		0
+	)
+	return {
+		...carData,
+		averageMileage: Math.round((carData.totalMileage / cars.length) * 100) / 100
+	}
+}
 
-module.exports = analyzeCarMileage;
+module.exports = analyzeCarMileage
