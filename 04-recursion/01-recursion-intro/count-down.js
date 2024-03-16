3323c1bf71a0995e685d37084f52d8aeffb91896
Jenna Tucker <jennatuckerdeveloper@gmail.com>
2024-03-15 23:46:03 -0700

    recursion/1

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,10 @@
-function countDown() {}
+const countDown = (num) => {
+	if (num <= 0) {
+		console.log('All done!')
+		return
+  }
+  console.log(num)
+	return countDown(num - 1)
+}
 
-module.exports = countDown;
+module.exports = countDown
