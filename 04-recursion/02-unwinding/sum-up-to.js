6664d75aee923fdc5c42ae555093ba9fdfbba124
Jenna Tucker <jennatuckerdeveloper@gmail.com>
2024-03-17 18:46:46 -0700

    recursion/2

--- 04-recursion/02-unwinding/sum-up-to.js
+++ 04-recursion/02-unwinding/sum-up-to.js
@@ -1,3 +1,7 @@
-function sumUpTo() {}
+const sumUpTo = (num) => {
+	if (num === 0) return 0
+	if (num === 1) return 1
+	return num + sumUpTo(num > 0 ? num - 1 : num + 1)
+}
 
-module.exports = sumUpTo;
+module.exports = sumUpTo
