0cef163d97e78bf61b7b90afce5d4c7161a9cc3f
Jenna Tucker <jennatuckerdeveloper@gmail.com>
2024-03-17 18:52:37 -0700

    recursion/3

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,7 @@
-function reverseString() {}
+const reverseString = (str) => {
+	if (str.length === 1) return str
+	const lastIndex = str.length - 1
+	return str[lastIndex] + reverseString(str.slice(0, lastIndex))
+}
 
-module.exports = reverseString;
+module.exports = reverseString
